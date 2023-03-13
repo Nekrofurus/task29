1192eb713c352c8037236f37fbe6f5a1e2e4e01e
VVCephei <vladgol90@gmail.com>
2023-03-13 15:40:43 +0200

    task 29

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ Console.Write("Введите минимальное значение = ");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите максимальное значение = ");
 int max = Convert.ToInt32(Console.ReadLine());
-int[] CreateRandomArray (int length, int min, int max)
+int[] CreateRandomArray(int length, int min, int max)
 {
     int[] array = new int[length];
     Random random = new Random();
-    for (int i = 0; i < length; i++)
+    for (int i = 1; i < length; i++)
     {
         array[i] = random.Next(min, max + 1);
     }
@@ -18,15 +18,15 @@ int[] CreateRandomArray (int length, int min, int max)
 }
 
 
-void PrintArray (int[] array)
+void PrintArray(int[] array)
 {
     Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 1; i < array.Length; i++)
     {
-       Console.Write($"{array[i]}, "); 
-    } 
-     Console.Write($"{array[array.Length - 1]}"); 
-     Console.Write("]");
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
 }
 int[] array = CreateRandomArray(length, min, max);
 PrintArray(array);
